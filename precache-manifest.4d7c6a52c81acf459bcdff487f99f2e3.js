8b8d7f9e40cb8aa53a419af08e85e08576fc8f8a
Santos Hernandez <santosdhernandez@gmail.com>
2019-12-02 00:49:21 -0700

    Updates

---
+++ precache-manifest.4d7c6a52c81acf459bcdff487f99f2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70cb374ce64c23d1b531f9c4432b7503",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f566e228e4bbe898fa3e",
+    "url": "/monsters-rolodex/static/css/main.1f3441e9.chunk.css"
+  },
+  {
+    "revision": "234c1c2ba8ada509f1df",
+    "url": "/monsters-rolodex/static/js/2.2332897c.chunk.js"
+  },
+  {
+    "revision": "f566e228e4bbe898fa3e",
+    "url": "/monsters-rolodex/static/js/main.6c1d14b9.chunk.js"
+  },
+  {
+    "revision": "7df5dc89bef5d0d8e5b8",
+    "url": "/monsters-rolodex/static/js/runtime-main.57b1bd7a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
